2db39bc5c44c3b1ab7a2c178189fb30cc2d57d4d
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-03-30 00:09:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.10 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr)
+{
+    var answer = [];
+    for(let i=0; i < arr.length; i++) {
+        if (arr[i] !== answer[answer.length -1]) {
+            answer.push(arr[i])
+        }
+    }
+    
+    return answer;
+}
