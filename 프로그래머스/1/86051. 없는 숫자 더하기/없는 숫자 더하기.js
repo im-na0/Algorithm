f574c3e5bcb968ac8af936dac1924486fddfd7ab
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-05-02 01:41:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var total = 45;  // 0부터 9까지의 합
+
+    for (var i = 0; i < numbers.length; i++) {
+        total -= numbers[i];  // 배열에 있는 숫자들을 하나씩 빼줌
+    }
+    
+    return total;  // 남은 합이 누락된 숫자들의 합
+}
