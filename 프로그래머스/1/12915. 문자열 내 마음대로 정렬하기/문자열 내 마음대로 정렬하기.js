120a6bc9fc764a453faf6bd0c8c6b5548867fa6c
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-22 00:00:14 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.94 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,12 @@
+
+function solution(strings, n) {
+    return strings.sort((a, b) => {
+        if (a[n] === b[n]) {
+            // n번째 문자가 같으면 전체 문자열을 사전순으로 비교
+            return a.localeCompare(b);
+        } else {
+            // n번째 문자가 다르면 그 문자를 기준으로 비교
+            return a[n].localeCompare(b[n]);
+        }
+    })
+}
