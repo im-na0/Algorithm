a93b768727c88a80de91717d9fd8edb5e4f6fd81
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-05 21:10:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 52.68 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,14 +1,20 @@
-function solution(s) {
-  const stack = [];
-
-  for (let i = 0; i < s.length; i++) {
-    const curr = s.charAt(i);
-    if (stack[stack.length - 1] === curr) {
-      stack.pop();
-    } else {
-      stack.push(curr);
-    }
-  }
-
-  return stack.length === 0 ? 1 : 0;
+function solution(s)
+{
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        // 스택이 비어있는지 검사
+        if (stack.length === 0) {
+            stack.push(s[i]);
+        } else {
+            // 현재값과 top 비교
+            if (stack[stack.length -1] === s[i]) {
+                stack.pop()
+            } else {
+                stack.push(s[i])
+            }
+        }
+    };
+    
+    return stack.length > 0 ? 0 : 1
 }
