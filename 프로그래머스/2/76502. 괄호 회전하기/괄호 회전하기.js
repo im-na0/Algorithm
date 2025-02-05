1793d07f24cd9c03a6bebe9846af2a10820e9435
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-05 19:23:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 12.81 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,27 +1,46 @@
 function solution(s) {
-  let count = 0;
-  const pairs = { ")": "(", "]": "[", "}": "{" };
-
-  for (let i = 0; i < s.length; i++) {
-    const str = s.slice(i) + s.slice(0, i);
-
-    const stack = [];
-    let isValid = true;
-    for (const c of str) {
-      if (c === "(" || c === "[" || c === "{") {
-        stack.push(c);
-      } else {
-        if (stack.length === 0 || stack.pop() !== pairs[c]) {
-          isValid = false;
-          break;
+    const n = s.length;
+    let answer = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const stack = [];
+        let isCorrect = true;
+        
+        for (let j = 0; j < n; j++) {
+            
+            // 문자열 회전
+            const c = s[(i + j) % n];
+            
+            if (c === "[" || c === "(" || c === "{") {
+                // 열린 괄호 푸시
+                stack.push(c)
+            } else {
+                if (stack.length === 0) {
+                    // 여는 괄호가 없는 경우
+                    isCorrect = false;
+                    break;
+                }
+                
+                // 닫힌 괄호가 스택의 top과 짝이 맞는지 비교
+                const top = stack[stack.length - 1];
+                if (c === "]" && top === "[") {
+                    stack.pop();
+                } else if (c === ")" && top === "(") {
+                    stack.pop();
+                    
+                } else if (c === "}" && top === "{") {
+                    stack.pop();
+                } else {
+                    isCorrect = false;
+                    break;
+                }
+            }
         }
-      }
+        
+        if (isCorrect && stack.length === 0) {
+            answer += 1;
+        }
     }
-
-    if (isValid && stack.length === 0) {
-      count++;
+        
+        return answer;
     }
-  }
-
-  return count;
-}
