bc0219e26a41609d4c1c9f1019290bb07a51717c
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-05 22:39:26 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,23 +1,36 @@
 function solution(board, moves) {
-    var answer = 0;
-    let basket = [];
     
-    moves.forEach(move => {
-        for(let i=0; i < board.length; i++) {
-            let doll = board[i][move - 1];
-            if (doll !== 0) {
-                if (basket[basket.length -1] === doll) {
-                    basket.pop();
-                    answer += 2;
-                } else {
-                    basket.push(doll)
-                }
-                board[i][move - 1] = 0;
-                break;
+    // 각 열에 대한 스택 생성
+    const lanes = [...Array(board[0].length)].map(() => []);
+    
+    // 역순으로, 각 열의 인형을 lanes에 추가
+    // [0, 0, 0, 4, 3] -> [3, 4]
+    for (let i = board.length -1; i >= 0; i--) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j]) {
+                lanes[j].push(board[i][j])
             }
         }
-        
-    });
+    }
+    
+    const bucket = [];
+    let answer = 0;
+    
+    for (const m of moves) {
+        if (lanes[m - 1].length > 0) {
+            // 해당 열에 인형이 있는 경우
+            
+            const doll = lanes[m - 1].pop();
+            
+            // 바구니에 인형이 있고, 가장 위에 있는 인형과 같은 경우
+            if (bucket.length > 0 && doll === bucket[bucket.length - 1]) {
+                bucket.pop();
+                answer += 2;
+            } else {
+                bucket.push(doll);
+            }
+        }
+    }
     
     return answer;
 }
