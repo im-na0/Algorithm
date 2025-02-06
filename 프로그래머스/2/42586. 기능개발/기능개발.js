2a12329a3575f9d2874685e0d16b2ba048953a28
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-06 22:36:07 +0900

    [level 2] Title: 기능개발, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,22 +1,29 @@
+// 1. 각 기능의 배포 예정일을 미리 구함
+// 2. 배포 가능일이 첫 번째 작업일 보다 빠른 작업들은 함께 배포
+// 3. 첫 번째 작업의 배포 가능일보다 늦은 작업이 나오면, 2단계와 유사하게 해당 작업의 배포일을 기준으로 뒤의 작업 배포
+
+
 function solution(progresses, speeds) {
-  const answer = [];
-  const days = progresses.map((progress, index) =>
-    Math.ceil((100 - progress) / speeds[index])
-  );
-  let count = 1;
-  let maxDay = days[0];
-
-  for (let i = 1; i < days.length; i++) {
-    if (days[i] <= maxDay) {
-      count++;
-    } else {
-      maxDay = days[i];
-      answer.push(count);
-      count = 1;
+    const answer = [];
+    const n = progresses.length;
+    
+    // 각 기능의 배포 예정일 구하기
+    const daysLeft = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    
+    let count = 0; // 배포될 작업 수 카운트
+    let maxDay = daysLeft[0]; // 가장 늦게 배포될 작업의 가능일
+    
+    for (let i = 0; i < n; i++) {
+        if (daysLeft[i] <= maxDay) {
+            count++;
+        } else {
+            // 배포 예정일이 기준 배포일보다 느리면
+            answer.push(count);
+            count = 1;
+            maxDay = daysLeft[i];
+        }
     }
-  }
-
-  answer.push(count);
-
-  return answer;
+    
+    answer.push(count);
+    return answer;
 }
