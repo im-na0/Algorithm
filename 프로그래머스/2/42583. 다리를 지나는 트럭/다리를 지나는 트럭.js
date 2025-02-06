fb23b4a4f9f1ae49eabef6d21c9abd9db14f218e
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-07 01:50:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 30.81 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,24 +1,54 @@
-function solution(bridge_length, weight, truck_weights) {
-   
-    let time = 0;
-    let bridge = new Array(bridge_length).fill(0);
-    let current_bridge = 0;
-    
-    while(truck_weights.length > 0 || current_bridge > 0) {
-        
-        time++;
-        current_bridge -= bridge.shift();
-        
-        if(truck_weights.length > 0 ){
-            if(truck_weights[0] + current_bridge <= weight){
-                const temp = truck_weights.splice(0,1)[0];
-                current_bridge += temp;
-                bridge.push(temp);
-            }else{
-                bridge.push(0);
-            }
-        }
+class Queue {
+    constructor(array = []) { 
+        this.items = array;
+        this.front = 0;
+        this.rear = array.length;
+        this.size = array.length;
     }
     
-    return time;
+    push(item) {
+        this.items.push(item);
+        this.rear++;
+        this.size++;
+    }
+    
+    pop() {
+        this.size--;
+        return this.items[this.front++];
+    }
+    
+    isEmpty() {
+        return this.front === this.rear;
+    }
+}
+
+function solution(bridge_length, weight, truck_weights) {
+    let answer = 0;
+    let current_weight = 0;
+    
+    truck_weights = new Queue(truck_weights);
+    const bridge = new Queue(); // 다리 위 트럭 큐 (무게, 경과 시간)
+    
+    while (!truck_weights.isEmpty() || bridge.size > 0) {
+        answer++; // 1초 증가
+
+        // 다리 위 트럭이 시간 초과로 나갈 수 있는지 체크
+        if (!bridge.isEmpty() && bridge.items[bridge.front][1] >= bridge_length) {
+            current_weight -= bridge.pop()[0]; // 트럭 무게 제거
+        }
+
+        // 대기 트럭이 다리 위로 올라갈 수 있는지 체크
+        if (!truck_weights.isEmpty() && current_weight + truck_weights.items[truck_weights.front] <= weight && bridge.size < bridge_length) {
+            let truck = truck_weights.pop();
+            bridge.push([truck, 0]); // 다리 위에 트럭 추가
+            current_weight += truck; // 다리 위 총 무게 업데이트
+        }
+
+        // 다리 위 트럭들의 경과 시간 증가
+        for (let i = bridge.front; i < bridge.rear; i++) {
+            bridge.items[i][1]++;
+        }
+    }
+
+    return answer;
 }
