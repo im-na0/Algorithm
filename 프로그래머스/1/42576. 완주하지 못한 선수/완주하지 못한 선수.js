9cae9a03b15d78cbab13102203dbe6f837b7a638
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-07 23:03:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.19 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,50 +1,26 @@
-// 문제 요구사항:
-// participant 배열 중 completion에 포함되지 않는 아이템을 찾아야함
+// 1. 참가자 이름을 해시 테이블에 추가하되, 키-값은 이름-개수로 설정
+// 2. 완주한 선수들의 이름을 해시 테이블에서 찾아 값을 1씩 줄임
+// 3. 1번에서 만든 해시를 순회해 값이 0이 아닌 키를 반환
 
-// 자료 구조 선택:
-// 데이터의 존재 여부를 파악해야함 -> 해시맵
 
-// 1. participant를 Map객체로 만듬
-// 2. 해당 객체에서 completion를 제외함
-// 3. 남은 key가 답!
-
-// Map으로 풀기: 실패 (시간 초과)
-// function solution(participant, completion) {
-//     const map = new Map();
-    
-//     for (const name of completion) {
-//         if (map.has(name)) {
-//             map.set(name, map.get(name) + 1);
-//         } else {
-//             map.set(name, 1);
-//         }
-//     }
-    
-//     for (const name of participant) {
-//         if (!map.has(name) || map.get(name) === 0) {
-//             return name
-//         }
-//         map.set(name, map.get(name) - 1);
-//     }
-// }
-
-// 객체로 풀기
 function solution(participant, completion) {
+    const obj = {};
     
-    const list = participant.reduce((total, cur) => {
-       total[cur] ? total[cur]++ : total[cur] = 1;
-        return total
-    }, {})
-    
-    for (var name of completion) {
-        if (list[name]) {
-            list[name]--;
+    for (const p of participant) {
+        if (obj[p]) {
+            obj[p] += 1;
+        } else {
+            obj[p] = 1;
         }
     }
     
-    for (var name in list) {
-        if (list[name] > 0) {
-            return name
+    for (const c of completion) {
+        obj[c] -= 1;
+    }
+    
+    for (const key in obj) {
+        if (obj[key] > 0) {
+            return key;
         }
     }
 }
