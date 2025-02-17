4c6cac12843562954e49245bfca09d09c437df25
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-02-17 23:06:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 247.52 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,40 +1,36 @@
+// 신고 테이블 생성 (신고당한 uid - 신고한 uid)
+// 테이블에서 값이 k번 이상인 이용정지 유저 찾기 
+// 이용정지 유저로 신고한 유저 찾아서 카운팅 처리
+
 function solution(id_list, report, k) {
     const reportObj = {};
     
-    // 신고 테이블 초기화
     for (const u of id_list) {
-        reportObj[u] = [];
+        reportObj[u] = []; 
     }
     
-    // 신고테이블에 신고받은 유저(key) - 신고자(value) 형식으로 저장
-    report.forEach(rep => {
-        const [reporter, reported] = rep.split(" ");
+    for (let i = 0; i < report.length; i++) {
+        const [reporter, reported] = report[i].split(" ");
         if (!reportObj[reported].includes(reporter)) {
             reportObj[reported].push(reporter);
         }
-    });
+    }
     
-    // 신고테이블의 각 값이 K번 이상인 경우 추출해서 이용정지 유저 찾기
-    suspendedUid = Object.keys(reportObj).filter((key) => {
+    // 신고 횟수가 k번 이상인 유저 찾기
+    const badUser = Object.keys(reportObj).filter((key) => {
         return reportObj[key].length >= k;
-    });
+    })
     
-    
-    // 이용정지 당한 유저를 신고한 유저를 찾아 카운팅처리
     const countObj = {};
-    id_list.forEach((uid) => {
-        countObj[uid] = 0;
-    });
+    for (const u of id_list) {
+        countObj[u] = 0;
+    }
     
-    
-    suspendedUid.forEach((uid) => {
+    badUser.forEach((uid) => {
         reportObj[uid].forEach((reporter) => {
-            countObj[reporter]++; 
+            countObj[reporter]++;
         })
     })
     
-    // id_list의 순서에 따라 결과 배열 생성
-    const answer = id_list.map(user => countObj[user]);
-    
-    return answer;
+    return id_list.map((uid) => countObj[uid]);
 }
