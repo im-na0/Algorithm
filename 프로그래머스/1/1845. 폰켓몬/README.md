5cd02e6269ef2f4d6b2e713336086eba55ee410e
im-na0 <139189221+im-na0@users.noreply.github.com>
2025-03-19 22:59:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.76 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 3.89 ms
+메모리: 34.3 MB, 시간: 0.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 23일 23:44:14
+2025년 03월 19일 22:59:24
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,30 +1,5 @@
-// 문제 요구사항:
-// N마리의 폰켓몬 중에서 N/2마리를 선택해야함.
-// 최대한 많은 종류의 폰켓몬을 선택하는 것이 목표.
-
-// 자료 구조 선택:
-// 각 폰켓몬의 종류를 파악해야함 (중복도 카운트) -> 해시맵
-
-// 1. 폰켓몬 종류를 구함
-// 2. 최대 선택 가능한 종류: 폰켓몬 종류와 N/2 중 적은 값이 답
-
 function solution(nums) {
-    const num = nums.length / 2;
-    let map = new Map();
-    
-
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            map.set(nums[i], map.get(nums[i]) + 1);
-        } else {
-            map.set(nums[i], 1);
-        }
-    }
-    
-    // table의 크기(서로 다른 폰켓몬의 수)와 num(N/2)을 비교
-    if (map.size < num) {
-        return map.size;
-    } else {
-        return num;
-    }
+    const numSet = new Set(nums);
+    const k = nums.length / 2;
+    return Math.min(k, numSet.size);
 }
